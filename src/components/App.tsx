ce3dc450639abdc83967822a73e0423054ebbd8f
swym123 <swayamrachhadia9@gmail.com>
2026-05-15 22:03:51 +0530

    done

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import Navbar from './Navbar'
+import ScrollVideoSection from './ScrollVideoSection'
+import SeedCompanySection from './SeedCompanySection'
+import EnergySection from './EnergySection'
+import Footer from './Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <ScrollVideoSection />
+      <SeedCompanySection />
+      <EnergySection />
+      <Footer />
+    </>
+  )
+}
